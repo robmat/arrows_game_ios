fba51a7b483e3e997d1d860b82d9b98ecf6b22e4
Robert Batorowski <Robert.Batorowski@redwood.com>
2026-02-11 14:29:38 +0100

    Initial Commit

---
+++ arrowsTests/arrowsTests.swift
@@ -0,0 +1,17 @@
+//
+//  arrowsTests.swift
+//  arrowsTests
+//
+//  Created by Robert Batorowski on 11/02/2026.
+//
+
+import Testing
+@testable import arrows
+
+struct arrowsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
